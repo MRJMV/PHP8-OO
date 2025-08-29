fcaeaa1763c274b3cb9bc6bd55bf0127b09dedab
MRJMV <earn_arctic.0k@icloud.com>
2025-08-29 14:11:18 +0200

    début de création de la page rpg

---
+++ classe2/Jean-Michael/bases-oo-RPG/01-class-base/Personnage.php
@@ -0,0 +1,8 @@
+<?php
+//le nom de la classe doit correspondre au nom du ficher
+//on déclare une classe avec le mot clef "class{}"
+class Personnage {
+    //Attributs
+    //Constantes
+    //Methodes
+}
